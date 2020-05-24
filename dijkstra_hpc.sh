6da0afb23c667c35711aeedd71bd8d6b6eceb257
Haoyao Chen <taylorchenca@gmail.com>
2020-05-23 17:43:22 -0700

    Added dijkstra_hpc.sh.

--- dijkstra_hpc.sh
+++ dijkstra_hpc.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+job_name=dijkstra_serial
 #
-#SBATCH --job-name=matmult
+#SBATCH --job-name=$job_name
 #SBATCH --partition=cpu
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=64G
 #SBATCH --nodes=1
-#SBATCH --output=matmult-%j.out #SBATCH --time=10:00
+#SBATCH --output=$job_name-%j.out 
+#SBATCH --time=23:59:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hchen7@scu.edu #
-export OMP_NUM_THREADS=1
-export OMP_PLACES=cores
-export OMP_PROC_BIND=spread
+
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-log_file_name=matmult-$current_time.log
-
-/WAVE/projects/COEN-319-Sp20/hchen7/hw3/dijkstra-parallel/dijkstra 1000 1000 1000  >> /WAVE/projects/COEN-319-Sp20/hchen7/hw2/logs/$log_file_name
-
-for size in 100 500 1000 5000 10000
-do
-  for source in
-  /WAVE/projects/COEN-319-Sp20/hchen7/hw3/dijkstra-parallel/dijkstra "/Users/kexinchen/SCU Google Drive/COEN 319/Homework/Homework 3 /test_data/${size}.graph" 0 "/Users/kexinchen/SCU Google Drive/COEN 319/Homework/Homework 3 /test_data/${size}_serial.out" >> "/Users/kexinchen/SCU Google Drive/COEN 319/Homework/Homework 3 /logs/$log_file_name"
+log_file_name=job_name-$current_time.log
+file_path=/WAVE/projects/COEN-319-Sp20/hchen7/hw3/ 
+for size in 100 500 1000 5000 10000; do
+    for k in `seq 1 20`; do   
+        source=$RANDOM
+        let "source %= $size"
+        printf $0, 1, $size , $k, $source ,
+        $file_path/dijkstra-parallel/dijkstra "$file_path/test_data/${size}.graph" $source "$file_path/test_data/${size}_serial.out" >> "$file_path/logs/$log_file_name"
+    done 
 done
